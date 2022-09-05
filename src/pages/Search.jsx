5fc89c8ae9d52e7f30f8596a597aca67bb332b09
Bruno Sayago <brunossgripp@gmail.com>
2022-09-05 19:24:37 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,36 @@ import React from 'react';
 import Header from '../Components/Header';
 
 export default class Search extends React.Component {
+  state = {
+    buttonEnabled: false,
+  };
+
+  verificaBotao = (evento) => {
+    if (evento.target.value.length >= 2) {
+      this.setState({ buttonEnabled: true });
+    }
+  };
+
   render() {
+    const { buttonEnabled } = this.state;
     return (
       <div data-testid="page-search">
         Search
         <Header />
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ this.verificaBotao }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ !buttonEnabled }
+          >
+            Buscar
+          </button>
+        </form>
       </div>
     );
   }
