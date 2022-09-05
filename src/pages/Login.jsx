700c5f90bb9419d65fa2e35c9f7ad65d13f32a63
Bruno Sayago <brunossgripp@gmail.com>
2022-09-05 18:55:19 -0300

    Requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Carregando from '../Components/Carregando';
 
 export default class Login extends React.Component {
+  state = {
+    username: '',
+    buttonEnabled: false,
+    loading: false,
+    redirect: false,
+  };
+
+  mudaNome = (evento) => {
+    this.setState({ username: evento.target.value });
+    const minLength = 3;
+    if (evento.target.value.length >= minLength) {
+      this.setState({ buttonEnabled: true });
+    } else {
+      this.setState({ buttonEnabled: false });
+    }
+  };
+
+  saveButton = async () => {
+    const { username } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name: username });
+    this.setState({ redirect: true });
+  };
+
   render() {
+    const { username, buttonEnabled, loading, redirect } = this.state;
+
     return (
-      <div data-testid="page-login">Login</div>
+      redirect ? <Redirect to="/search" />
+        : (
+          <div data-testid="page-login">
+            { !loading
+              ? (
+                <form>
+                  <input
+                    value={ username }
+                    data-testid="login-name-input"
+                    type="text"
+                    onChange={ this.mudaNome }
+                  />
+                  <button
+                    data-testid="login-submit-button"
+                    type="button"
+                    disabled={ !buttonEnabled }
+                    onClick={ this.saveButton }
+                  >
+                    Entrar
+                  </button>
+                </form>
+              )
+              : <Carregando />}
+          </div>
+        )
+
     );
   }
 }
