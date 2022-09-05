745ed65caa497cc862a4ff647176bc3c69c82134
Bruno Sayago <brunossgripp@gmail.com>
2022-09-05 20:15:06 -0300

    Requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,37 +1,90 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Carregando from '../Components/Carregando';
 
 export default class Search extends React.Component {
   state = {
+    busca: '',
+    carregando: false,
+    carregado: false,
+    artistaBuscado: '',
     buttonEnabled: false,
+    retorno: '',
   };
 
-  verificaBotao = (evento) => {
-    if (evento.target.value.length >= 2) {
+  mudaPesquisa = ({ target }) => {
+    this.setState({ busca: target.value });
+    if (target.value.length >= 2) {
       this.setState({ buttonEnabled: true });
     }
   };
 
+  buttonClick = async (artista) => {
+    this.setState({ carregando: true, artistaBuscado: artista });
+    const retorno = await searchAlbumsAPI(artista);
+    this.setState({ carregado: true, retorno, carregando: false, busca: '' });
+  };
+
+  dadosTratados = (retorno) => {
+    if (retorno.length === 0) {
+      return (
+        <p>Nenhum álbum foi encontrado</p>
+      );
+    }
+    return retorno.map(({ collectionName, artistName, collectionId }) => (
+      <Link
+        key={ collectionId }
+        to={ `/album/${collectionId}` }
+        data-testid={ `link-to-album-${collectionId}` }
+      >
+        <p>{ collectionName }</p>
+        <p>{artistName}</p>
+      </Link>
+    ));
+  };
+
   render() {
-    const { buttonEnabled } = this.state;
+    const {
+      busca,
+      carregando,
+      carregado,
+      artistaBuscado,
+      buttonEnabled,
+      retorno,
+    } = this.state;
+
+    const achouAlbuns = retorno.length > 0;
+
+    const resultado = <p>{`Resultado de álbuns de: ${artistaBuscado}`}</p>;
+
+    const inicial = (
+      <form>
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          onChange={ this.mudaPesquisa }
+          value={ busca }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ !buttonEnabled }
+          onClick={ () => this.buttonClick(busca) }
+        >
+          Buscar
+        </button>
+      </form>
+    );
+
     return (
       <div data-testid="page-search">
-        Search
         <Header />
-        <form>
-          <input
-            type="text"
-            data-testid="search-artist-input"
-            onChange={ this.verificaBotao }
-          />
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ !buttonEnabled }
-          >
-            Buscar
-          </button>
-        </form>
+        {!carregando && inicial}
+        {carregando && <Carregando />}
+        {!carregando && carregado && achouAlbuns && resultado}
+        {carregado && this.dadosTratados(retorno)}
       </div>
     );
   }
